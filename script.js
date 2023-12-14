7dcb218fc99066a14593320a2fcec7bb06b74f82
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2023-12-14 10:55:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const modal = document.getElementById("myModal")
+const button = document.getElementById("openModal")
+
+document.addEventListener('click',(e)=>{
+	if(!modal.contains(e.target) && !button.contains(e.target)){
+		modal.style.display = "none"
+	}
+})
+
+button.addEventListener('click',()=>{
+	modal.style.display = "block"	
+})
